b54b58c57eb5f35f5b21c8b2fd97390677b1cdfc
prubioamzubiri <prubioam@zubirimanteo.com>
2023-10-18 18:35:22 +0200

    Añadidas pruebas de paso por valor o referencias

---
+++ Pruebas/Algo.java
@@ -0,0 +1,20 @@
+package Pruebas;
+public class Algo {
+
+    private int numero;
+
+    public Algo(){
+        numero = 2;
+    }
+
+    public void sumar()
+    {
+        numero++;
+    }
+
+    public int getNumero()
+    {
+        return numero;
+    }
+    
+}
---
+++ Pruebas/PasoValorReferencia.java
@@ -0,0 +1,37 @@
+package Pruebas;
+
+public class PasoValorReferencia {
+
+    public static void main(String[] args)
+    {
+
+        int j = 0;
+        paso(j); // j++
+        System.out.println(j); // muestra 0
+
+        int[] k = {1,2};
+        pasoArray(k); // k[0]++
+        System.out.println(k[0]); //muestra 2
+
+        Algo algo = new Algo(); //empieza en 2
+        pasoObjeto(algo); //suma 1 al atributo
+        System.out.println(algo.getNumero()); // devuelve 3
+
+    }
+
+    public static void paso(int a)
+    {
+        a++;
+    }
+
+    public static void pasoArray(int[] b)
+    {
+        b[0]++;
+    }
+
+    public static void pasoObjeto(Algo al)
+    {
+        al.sumar();
+    }
+    
+}
