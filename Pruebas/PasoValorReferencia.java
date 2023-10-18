6fee5c35ce61d8c597b7a5f406eb58de1de3dd5a
prubioamzubiri <prubioam@zubirimanteo.com>
2023-10-18 18:37:34 +0200

    comentarios añadidos

--- Pruebas/PasoValorReferencia.java
+++ Pruebas/PasoValorReferencia.java
@@ -4,15 +4,17 @@ public class PasoValorReferencia {
 
     public static void main(String[] args)
     {
-
+        //paso valor
         int j = 0;
         paso(j); // j++
         System.out.println(j); // muestra 0
 
+        //paso referencia
         int[] k = {1,2};
         pasoArray(k); // k[0]++
         System.out.println(k[0]); //muestra 2
 
+        //paso referencia
         Algo algo = new Algo(); //empieza en 2
         pasoObjeto(algo); //suma 1 al atributo
         System.out.println(algo.getNumero()); // devuelve 3
